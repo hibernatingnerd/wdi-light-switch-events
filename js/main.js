1aca79b29f2b51c890c717842798477e9baba860
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-07-16 14:15:41 -0400

    functioning javascript run light switch page.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  var switchSwap = document.querySelector('button.switch.on');
+  // function flipTheSwitch() {
+  //   if (switchSwap.className="switch on") {
+  //     switchSwap.className="switch off";
+  //   } else if {
+  //     switchSwap.className="switch on";
+  //   }
+  // }
+  switchSwap.addEventListener('click', function() {
+    if (switchSwap.className === "switch on") {
+      switchSwap.className = "switch off";
+      document.querySelector('body').className = "dark";
+      document.querySelector('body .status').innerHTML = "My gosh it's pretty dark in here.";
 
+    } else if (switchSwap.className === "switch off") {
+      switchSwap.className = "switch on";
+      document.querySelector('body .status').innerHTML = "It's so bright in here!";
+      document.querySelector('body').className = "light";
+    }
+  });
+
+});
